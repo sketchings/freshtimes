6adc7b8bff3e359cfac3610aab30e6f214995b93
Alena Holligan <alena@holligan.us>
2017-07-21 10:10:03 -0700

    Funding Thermometer

--- public/balance.php
+++ public/balance.php
@@ -1,10 +1,33 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = new Symfony\Component\Dotenv\Dotenv();
+use Symfony\Component\Dotenv\Dotenv;
+$dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../.env');
-
 $paypal = new Freshtimes\Paypal(getenv("USERNAME"), getenv("PASSWORD"), getenv("SIGNATURE"), getenv("ENVIRONMENT"));
 $response = $paypal->call('GetBalance');
+?>
+<html>
+<head>
+    <title>Fresh Times Family Reunion Goal</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
-echo '<h1>Coming Soon... Fresh Times!</h1>';
-echo '<p>Current Balance: $' . $response['L_AMT0'] . '</p>';
+</head>
+<body>
+
+<div id="thermometer">
+
+    <div class="track">
+        <div class="goal">
+            <div class="amount"> 10000 </div>
+        </div>
+        <div class="progress">
+            <div class="amount"> <?php echo $response['L_AMT0']; ?> </div>
+        </div>
+    </div>
+
+</div>
+
+<script src="js/script.js"></script>
+</body>
+</html>
