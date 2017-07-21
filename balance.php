9a4879269054cba758eefa9d57e7f99d19b8f942
Alena Holligan <alena@holligan.us>
2017-07-21 00:43:14 -0700

    initial commit
    Placeholder with PayPal balance

---
+++ balance.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+$paypal = new Freshtimes\Paypal(getenv("USERNAME"), getenv("PASSWORD"), getenv("SIGNATURE"), getenv("ENVIRONMENT"));
+$response = $paypal->call('GetBalance');
+
+echo '<h1>Coming Soon... Fresh Times!</h1>';
+echo '<p>Current Balance: $' . $response['L_AMT0'] . '</p>';
